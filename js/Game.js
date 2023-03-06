6bd3ecfe0fcba17f3361f84a313149eda646243b
anujkumar072010 <117281207+anujkumar072010@users.noreply.github.com>
2023-03-06 17:28:59 +0530

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -1,20 +1,18 @@
 class Game {
   constructor() {}
-  //BP
+
   getState() {
     var gameStateRef = database.ref("gameState");
     gameStateRef.on("value", function(data) {
       gameState = data.val();
     });
   }
-  //BP
   update(state) {
     database.ref("/").update({
       gameState: state
     });
   }
 
-  // TA
   start() {
     player = new Player();
     playerCount = player.getCount();
@@ -22,50 +20,53 @@ class Game {
     form = new Form();
     form.display();
 
-    car1 = createSprite(width / 2 - 50, height - 100);
-    car1.addImage("car1", car1_img);
-    car1.scale = 0.07;
+    animal1 = createSprite(width / 2 - 50, height - 100);
+    animal1.addImage(" animal1",  animal1_img);
+    animal1.scale = 0.07;
 
-    car2 = createSprite(width / 2 + 100, height - 100);
-    car2.addImage("car2", car2_img);
-    car2.scale = 0.07;
+    animal2 = createSprite(width / 2 + 100, height - 100);
+    animal2.addImage(" animal2",  animal2_img);
+    animal2.scale = 0.07;
 
-    cars = [car1, car2];
-    
-    fuels = new Group();
-    powerCoins = new Group(); 
+    animals = [ animal1,  animal2];
 
-    this.addSprites(fuels,4,fuelImage,0.02);
-    this.addSprites(powerCoins,18,powerCoinImage,0.09);
-    
+    // C38 TA
+    bush = new Group();
+    grass = new Group();
+
+    // Adding fuel sprite in the game
+    this.addSprites(bush, 4, bushImage, 0.02);
+
+    // Adding coin sprite in the game
+    this.addSprites(grass, 18, grassImage, 0.09);
   }
 
-  //BP
+  // C38 TA
+  addSprites(spriteGroup, numberOfSprites, spriteImage, scale) {
+    for (var i = 0; i < numberOfSprites; i++) {
+      var x, y;
+
+      x = random(width / 2 + 150, width / 2 - 150);
+      y = random(-height * 4.5, height - 400);
+
+      var sprite = createSprite(x, y);
+      sprite.addImage("sprite", spriteImage);
+
+      sprite.scale = scale;
+      spriteGroup.add(sprite);
+    }
+  }
+
   handleElements() {
     form.hide();
     form.titleImg.position(40, 50);
     form.titleImg.class("gameTitleAfterEffect");
   }
 
-  addSprites(spriteGroup,numberOfSprites,spriteImage,scale){
-
-    for(var i = 0; i < numberOfSprites;i++) {
-      var x,y
-      x = random(width /2 + 150,width /2 - 150) 
-      y = random(-height * 4.5,height-400)
-
-      var sprite = createSprite(x,y)
-      sprite.addImage("sprite",spriteImage)
-      sprite.scale = scale()
-      spriteGroup.add(sprite)
-    }
-  }
-
-  //SA
   play() {
     this.handleElements();
 
-    Player.getPlayersInfo(); //added
+    Player.getPlayersInfo();
 
     if (allPlayers !== undefined) {
       image(track, 0, -height * 5, width, height * 6);
@@ -73,24 +74,27 @@ class Game {
       //index of the array
       var index = 0;
       for (var plr in allPlayers) {
+        //add 1 to the index for every loop
+        index = index + 1;
+
         //use data form the database to display the cars in x and y direction
         var x = allPlayers[plr].positionX;
         var y = height - allPlayers[plr].positionY;
 
-        cars[index].position.x = x;
-        cars[index].position.y = y;
+        animals[index - 1].position.x = x;
+        animals[index - 1].position.y = y;
 
-        //add 1 to the index for every loop
-        index = index + 1;
-
+        // C38  SA
         if (index === player.index) {
           stroke(10);
           fill("red");
           ellipse(x, y, 60, 60);
 
-          this.handleFuel(index)
-          this.handlePowerCoins(index)
-        }
+          this.handleBush(index);
+          this.handleGrass(index);
+
+          // Changing camera position in y direction
+         }
       }
 
       // handling keyboard events
@@ -98,25 +102,44 @@ class Game {
         player.positionY += 10;
         player.update();
       }
- 
+
       drawSprites();
     }
   }
-  handleFuel(index){
 
-    cars[index - 1].overlap(fuels,function(collecter, collected){
-    player.fuel = 185;
-    
-    collected.remove();
-     } )
+  handleBush(index) {
+    // Adding fuel
+    animals[index - 1].overlap(bush, function(collector, collected) {
+      player.bush = 185;
+      //collected is the sprite in the group collectibles that triggered
+      //the event
+      collected.remove();
+    });
   }
 
-  handlePowerCoins(index){
-    cars[index - 1].overlap(powerCoins,function(collector,collected){
-      player.score += 21
-      player.update()
-      collected.remove()
-  })
-}
-  
+  handleGrass(index){
+
+     animals[index - 1].overlap(grass, function(collector, collected) {
+       player.score += 21;
+       player.update();
+       collected.remove();
+     });
+
+
+  //   animals[index - 1].overlap(grass, function(collected, collector) {
+  //     player.score += 21;
+  //     player.update();
+  //     collector.remove();
+  //   });
+
+
+  //   animals[index].overlap(grass, function(collector, collected) {
+  //     player.score += 21;
+  //     player.update();
+  //     collected.update();
+  //   });
+
+  }
+
+    
 }
